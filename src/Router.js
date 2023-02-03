0b0954a63f4d8791ae08c906d4218259f2f2d38d
gimcheolho <sm708311@gmail.com>
2023-02-03 11:23:38 +0900

    Add: 예약내역페이지 레이아웃 및 기능구현

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import Booking from './pages/Booking/Booking';
+import BookingComplete from './pages/BookingComplete';
 import Detail from './pages/Detail/Detail';
 import Hosting from './pages/Hosting';
 import Payment from './pages/Payment';
@@ -16,6 +17,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/bookingcomplete" element={<BookingComplete />} />
         <Route path="/Detail" element={<Detail />} />
         <Route path="/hosting" element={<Hosting />} />
         <Route path="/payment" element={<Payment />} />
